40a1f6f237e23201dbefebe7a10da14d96d6ce6f
xa <xaywj30@gmail.com>
2025-10-26 00:51:43 +0700

    add method put

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.use(helmet({
 // Configure CORS
 app.use(cors({
   origin: "*",
-  methods: ['GET', 'POST','DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST','DELETE','PUT', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
